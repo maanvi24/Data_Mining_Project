307e0f7591cdd8f3bbe0fdb2d13ded7aac8cd925
gmon <k.g@northeastern.edu>
2023-12-02 17:35:21 -0800

    Update App.js
    addition of the nav bar and other pages

--- Website/src/App.js
+++ Website/src/App.js
@@ -1,14 +1,27 @@
 // src/App.js
 import React from 'react';
-import ArticleForm from './ArticleForm';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './Pages/Navbar';
+import Home from './ArticleForm';
+import About from './Pages/About';
+import LiveFeed from './Pages/LiveFeed';
+import Summary from './Pages/Summary';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Stock Prediction App</h1>
-      <ArticleForm />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/live-feed" element={<LiveFeed />} />
+          <Route path="/summary" element={<Summary />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
