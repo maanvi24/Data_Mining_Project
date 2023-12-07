ed72d195874d7fb1adcbb4e494797e82bbde205c
gmon <k.g@northeastern.edu>
2023-12-06 23:54:48 -0800

    UPDATE LIVEFEED

--- Website/src/Pages/LiveFeed.js
+++ Website/src/Pages/LiveFeed.js
@@ -1,137 +1,163 @@
+
 import React, { useState } from 'react';
+import axios from 'axios';
+const styles = {
+  liveFeedContainer: {
+    margin: '20px',
+    marginTop: '70px', 
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: '20px',
+  },
+  label: {
+    marginBottom: '10px',
+  },
+  input: {
+    marginBottom: '10px',
+    padding: '8px',
+  },
+  select: {
+    marginBottom: '10px',
+    padding: '8px',
+  },
+  button: {
+    marginBottom: '10px',
+    padding: '8px',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s',
+  },
+  link: {
+    color: '#007BFF',
+    textDecoration: 'underline',
+    cursor: 'pointer',
+  },
+  errorMessage: {
+    color: 'red',
+  },
+  table: {
+    width: '100%',
+    borderCollapse: 'collapse',
+    marginTop: '20px',
+  },
+  th: {
+    border: '1px solid #ddd',
+    padding: '8px',
+    textAlign: 'left',
+    backgroundColor: '#f2f2f2',
+  },
+  td: {
+    border: '1px solid #ddd',
+    padding: '8px',
+    textAlign: 'left',
+  },
+};
 
-// Define the LiveFeed component
 const LiveFeed = () => {
-  // State hooks for managing input, articles, and error
   const [ticker, setTicker] = useState('');
-  const [selectedInterval, setSelectedInterval] = useState('1d');
+  const [interval, setInterval] = useState('1d');
   const [articles, setArticles] = useState([]);
-  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  // Function to fetch articles based on user input
-  const getArticles = async () => {
-    try {
-      const response = await fetch('http://127.0.0.1:5000/get_articles', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ ticker, selected_interval: selectedInterval }),
-      });
-
-      const result = await response.json();
-
-      if (result.success) {
-        setArticles(result.articles);
-        setError('');
-      } else {
-        setArticles([]);
-        setError(result.error);
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      setArticles([]);
-      setError('An error occurred while fetching data.');
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'ticker') {
+      setTicker(value);
+    } else if (name === 'interval') {
+      setInterval(value);
     }
   };
 
-  // JSX structure for rendering the component
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    try {
+      // Simulate delay for loading
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+
+      const response = await axios.post('http://127.0.0.1:5000/get_articles', { ticker, interval });
+      setArticles(response.data.articles);
+    } catch (error) {
+      setError('Error fetching articles. Please try again.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="Livefeed">
-      {/* Inline styles for styling purposes */}
-      <style>
-        {`
-          .Livefeed {
-            text-align: center;
-          }
+    <div style={styles.liveFeedContainer}>
+      <form style={styles.form} onSubmit={handleSubmit}>
+        <label style={styles.label}>
+          Ticker:
+          <input
+            type="text"
+            name="ticker"
+            value={ticker}
+            onChange={handleInputChange}
+            style={styles.input}
+            required
+          />
+        </label>
+        <label style={styles.label}>
+          Interval:
+          <select name="interval" value={interval} onChange={handleInputChange} style={styles.select}>
+            <option value="1d">1 Day</option>
+            <option value="1w">1 Week</option>
+            <option value="1m">1 Month</option>
+            <option value="3m">3 Months</option>
+            <option value="6m">6 Months</option>
+          </select>
+        </label>
+        <button type="submit" disabled={loading} style={styles.button}>
+          {loading ? 'Loading...' : 'Get Articles'}
+        </button>
+      </form>
 
-          .Livefeed-header {
-            background-color: #282c34;
-            padding: 30px;
-            color: white;
-          }
+      {error && <p style={styles.errorMessage}>{error}</p>}
 
-          .Livefeed-content {
-            margin-top: 20px;
-          }
-
-          label {
-            margin-right: 10px;
-          }
-
-          input,
-          select,
-          button {
-            margin-bottom: 10px;
-          }
-
-          /* Style for each article box */
-          .article-box {
-            border: 1px solid #ccc;
-            padding: 10px;
-            margin-bottom: 10px;
-          }
-        `}
-      </style>
-      {/* Header */}
-      <header className="Livefeed-header">
-        <h1>Live Feed App</h1>
-      </header>
-      {/* Content */}
-      <div className="Livefeed-content">
-        {/* Input and selection elements */}
-        <label htmlFor="ticker">Enter Ticker:</label>
-        <input
-          type="text"
-          id="ticker"
-          placeholder="Enter Ticker"
-          value={ticker}
-          onChange={(e) => setTicker(e.target.value)}
-        />
-
-        <label htmlFor="interval">Select Interval:</label>
-        <select
-          id="interval"
-          value={selectedInterval}
-          onChange={(e) => setSelectedInterval(e.target.value)}
-        >
-          <option value="1d">1 Day</option>
-          <option value="1w">1 Week</option>
-          <option value="1m">1 Month</option>
-          <option value="3m">3 Months</option>
-          <option value="6m">6 Months</option>
-        </select>
-
-        {/* Button to trigger fetching of articles */}
-        <button onClick={getArticles}>Get Live Feed</button>
-
-        {/* Display articles */}
-        <div id="result">
-          {error && <p>Error: {error}</p>}
-          {articles.length > 0 && (
-            <>
-              <h2>Articles:</h2>
-              {/* Map over articles and create a styled box for each */}
-              {articles.map((article, index) => (
-                <div key={index} className="article-box">
-                  <p>
-                    <strong>{article.title}</strong> - {article.date_published}
-                  </p>
-                  <p>{article.summary}</p>
-                  <p>
-                    <a href={article.url} target="_blank" rel="noopener noreferrer">
-                      Read More
+      {articles.length > 0 && (
+        <table style={styles.table}>
+          <thead>
+            <tr>
+              <th style={styles.th}>URL</th>
+              <th style={styles.th}>Date Published</th>
+              <th style={styles.th}>Likelihood</th>
+              <th style={styles.th}>Model Prediction</th>
+              <th style={styles.th}>Actual</th>
+              <th style={styles.th}>Open Price</th>
+              <th style={styles.th}>Close Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {articles.map((article, index) => (
+              <React.Fragment key={index}>
+                <tr>
+                  <td style={styles.td}>
+                    <a href={article.URL} target="_blank" rel="noopener noreferrer" style={styles.link}>
+                      Click Here
                     </a>
-                  </p>
-                </div>
-              ))}
-            </>
-          )}
-        </div>
-      </div>
+                  </td>
+                  <td style={styles.td}>{article['Date Published']}</td>
+                  <td style={styles.td}>{article.Likelihood}</td>
+                  <td style={styles.td}>{article['Model Prediction']}</td>
+                  <td style={styles.td}>{article.Actual}</td>
+                  <td style={styles.td}>{article['Open Price']}</td>
+                  <td style={styles.td}>{article['Close Price']}</td>
+                </tr>
+                <tr>
+                  <td colSpan="7" style={{ height: '10px' }}></td>
+                </tr>
+              </React.Fragment>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
 
-// Export the LiveFeed component
 export default LiveFeed;
