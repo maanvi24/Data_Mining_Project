dbd671de57a86883dea4afbf7738bcb6bc8ac7e0
patelarjun1 <patel.arjun1@northeastern.edu>
2023-12-06 22:26:01 -0800

    adding up summarizer

--- Website/src/Pages/Summary.js
+++ Website/src/Pages/Summary.js
@@ -1,57 +1,67 @@
 import React, { useState } from 'react';
-
+ 
 const Summary = () => {
   const [article, setArticle] = useState('');
-  const [numLines, setNumLines] = useState(5); // Default number of lines
+  const [maxLength, setMaxLength] = useState(500); // Default max length
+  const [minLength, setMinLength] = useState(10); // Default min length
+  const [numSentences, setNumSentences] = useState(2); // Default number of sentences
   const [generatedSummary, setGeneratedSummary] = useState('');
-
+  const [error, setError] = useState(null);
+ 
   const handleGenerateSummary = async () => {
     try {
-      const response = await fetch('http://localhost:5001/generate_summary', {
+      const response = await fetch('http://127.0.0.1:5000/generate_summary', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           summary: article,
-          num_lines: numLines,
+          max_length: maxLength,
+          min_length: minLength,
+          num_sentences: numSentences,
         }),
       });
-
+ 
+      if (!response.ok) {
+        throw new Error('Failed to generate summary');
+      }
+ 
       const data = await response.json();
       setGeneratedSummary(data.generated_summary);
+      setError(null); // Reset any previous errors
     } catch (error) {
       console.error('Error generating summary:', error);
+      setGeneratedSummary(''); // Reset the summary on error
+      setError('Error generating summary. Please try again.'); // Set an error message
     }
   };
-
+ 
   const styles = {
     container: {
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
-      justifyContent: 'center',
-      minHeight: '100vh',
-      backgroundColor: '#033673', // Blue background
-      color: '#000', // Text color
+      padding: '20px',
+      backgroundColor: '#333',
+      color: '#fff',
     },
     header: {
       fontSize: '24px',
       marginBottom: '20px',
-      color: '#fff',
     },
     textarea: {
-      width: '80%',
+      width: '100%',
       height: '150px',
       padding: '10px',
-      margin: '10px 0',
+      marginBottom: '20px',
       border: '1px solid #ccc',
       borderRadius: '4px',
+      resize: 'vertical',
     },
     label: {
       fontSize: '16px',
       marginBottom: '5px',
-      color: '#fff',
     },
     input: {
       width: '50px',
@@ -61,47 +71,65 @@ const Summary = () => {
       borderRadius: '4px',
     },
     button: {
-      backgroundColor: '#4caf50',
+      padding: '10px',
+      backgroundColor: '#007bff',
       color: '#fff',
-      padding: '10px 15px',
       border: 'none',
       borderRadius: '4px',
       cursor: 'pointer',
     },
     summaryContainer: {
+      marginTop: '20px',
+      textAlign: 'left', // Adjusted for better readability
+    },
+    error: {
+      color: 'red',
       marginTop: '10px',
-      border: '1px solid #ccc',
-      padding: '10px',
     },
   };
-
+ 
   return (
-    <div style={styles.container}>
-      <h1 style={styles.header}>Summary Generator</h1>
-      <textarea
+<div style={styles.container}>
+<h1 style={styles.header}>Summary Generator</h1>
+<textarea
         style={styles.textarea}
         placeholder="Enter your article here..."
         value={article}
         onChange={(e) => setArticle(e.target.value)}
-      ></textarea>
-      <label style={styles.label}>Number of Lines for Summary:</label>
-      <input
+></textarea>
+<label style={styles.label}>Max Length for Summary:</label>
+<input
         style={styles.input}
         type="number"
-        value={numLines}
-        onChange={(e) => setNumLines(e.target.value)}
+        value={maxLength}
+        onChange={(e) => setMaxLength(e.target.value)}
       />
-      <button style={styles.button} onClick={handleGenerateSummary}>
+<label style={styles.label}>Min Length for Summary:</label>
+<input
+        style={styles.input}
+        type="number"
+        value={minLength}
+        onChange={(e) => setMinLength(e.target.value)}
+      />
+<label style={styles.label}>Number of Sentences for Summary:</label>
+<input
+        style={styles.input}
+        type="number"
+        value={numSentences}
+        onChange={(e) => setNumSentences(e.target.value)}
+      />
+<button style={styles.button} onClick={handleGenerateSummary}>
         Generate Summary
-      </button>
+</button>
+      {error && <p style={styles.error}>{error}</p>}
       {generatedSummary && (
-        <div style={styles.summaryContainer}>
-          <h2 style={{ color: '#fff' }}>Generated Summary:</h2>
-          <p>{generatedSummary}</p>
-        </div>
+<div style={styles.summaryContainer}>
+<h2>Generated Summary:</h2>
+<p>{generatedSummary}</p>
+</div>
       )}
-    </div>
+</div>
   );
 };
-
+ 
 export default Summary;
